2d5dd27f48f7b9ec50503b54998d0fabb80b6d92
Tessa300 <tessa.steinigke@heilbronn.dhbw.de>
2025-04-27 19:58:46 +0200

    Datenbank doch wieder umbenannt

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname' => '127.0.0.1',
         'username' => 'root',
         'password' => '',
-        'database' => 'vs25_products',
+        'database' => 'db_kitchen',
         'DBDriver' => 'MySQLi',       
         'DBPrefix'     => '',
         'pConnect'     => false,
