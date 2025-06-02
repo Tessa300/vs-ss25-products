6c40f69e57a45ebe7aab239ab1ec8f5c25f59ae7
Tessa300 <tessa.steinigke@heilbronn.dhbw.de>
2025-06-02 03:52:50 +0200

    VL3 Nachbereitung (Produkte bearbeiten vollständig und schöner)

--- app/Controllers/Products.php
+++ app/Controllers/Products.php
@@ -9,63 +9,55 @@ class Products extends BaseController
         $this->MainModel = new MainModel();
     }
 
-
-    public function getIndex($product_type_id = null)
-    {
-        if(is_null($product_type_id)){            
+    private function returnView($product_type_id = null, $data = []){
+        if(is_null($product_type_id)) {
             $data['products'] = $this->MainModel->getProducts();
 
             echo view("templates/header", ['title' => 'Produkte', 'description' => 'Anzeige aller Produkte']);
             echo view("pages/products", $data);
             echo view("templates/footer");
         }else{
-            $data['product'] = $this->MainModel->getProduct($product_type_id);
-            $data['product_ingredients'] = $this->MainModel->getProductIngredients($product_type_id);
-                
-            echo view("templates/header", ['title' => 'Produkte', 'description' => 'Anzeige aller Produkte']);
+            $seo = ['title' => 'Produkt', 'description' => 'Hinzufügen eines neuen Produkts'];
+            if($product_type_id != 'new') {
+                $data['product'] = $this->MainModel->getProduct($product_type_id);
+                $data['product_ingredients'] = $this->MainModel->getProductIngredients($product_type_id);
+                $seo['description'] = 'Anzeige des Produkts '.$data['product']['name'];
+            }
+            $data['unit_symbols'] = ['kg', 'l', 'Stk.', 'Port.'];
+            $data['ingredients'] = $this->MainModel->getIngredients();
+
+            echo view("templates/header", $seo);
             echo view("pages/product", $data);
             echo view("templates/footer");
-
         }
-        
+    }    
+
+    public function getIndex($product_type_id = null){
+        $this->returnView($product_type_id);
     }
 
     public function postUpdate($product_type_id){
-        $data=[];
-        if($this->validation->run($_POST, 'product')){
-            //echo "Richtig!";
+        if ($this->validation->run($_POST, 'product')) {
             $_POST['product_type_id'] = $product_type_id;
             $this->MainModel->updateProduct($_POST);
-        }else{
-            $data['errors'] = $this->validation->getErrors();
+            return redirect()->to(site_url('products/'.$product_type_id));
+        } else {
+            // Fehlermeldungen generieren
+            $data['error'] = $this->validation->getErrors();
+            $this->returnView($product_type_id, $data);
         }
-
-        //var_dump($_POST);
-
-        $data['product'] = $this->MainModel->getProduct($product_type_id);
-        $data['product_ingredients'] = $this->MainModel->getProductIngredients($product_type_id);
-                
-        echo view("templates/header", ['title' => 'Produkte', 'description' => 'Anzeige aller Produkte']);
-        echo view("pages/product", $data);
-        echo view("templates/footer");
-
     }
 
     public function postUpdateingredient($product_type_id){
         if ($this->validation->run($_POST, 'ingredient')) {
             $_POST['product_type_id'] = $product_type_id;
             $this->MainModel->updateIngredient($_POST);
+            return redirect()->to(site_url('products/'.$product_type_id));
         } else {
             // Fehlermeldungen generieren
             $data['error'] = $this->validation->getErrors();
+            $this->returnView($product_type_id, $data);
         }
-
-        $data['product'] = $this->MainModel->getProduct($product_type_id);
-        $data['product_ingredients'] = $this->MainModel->getProductIngredients($product_type_id);
-                
-        echo view("templates/header", ['title' => 'Produkte', 'description' => 'Anzeige aller Produkte']);
-        echo view("pages/product", $data);
-        echo view("templates/footer");
     }
 
 
--- app/Views/pages/product.php
+++ app/Views/pages/product.php
@@ -6,18 +6,93 @@
 
 <form method="POST" action="<? echo site_url('products/update/'.$product['product_type_id']) ?>" >
 
-    name: <input type="text" name="name" value="<? echo $product['name']?>">
-    <br>
-    preis: <input type="text" name="price_per_unit" value="<? echo $product['price_per_unit'] ?>">
-    <? echo isset($errors['price_per_unit']) ? $errors['price_per_unit'] : '' ?>
+    <div class="form-group mb-2 row">
+        <label for="name" class="col-sm-2 col-form-label">Name</label>
+        <div class="col-sm-10">
+            <input type="text" class="form-control <?= isset($error['name'])?'is-invalid':'' ?>" id="name" name="name"
+                   placeholder="Name"
+                   value="<?= isset($product['name']) ? $product['name'] : ''; ?>" >
+            <div class="invalid-feedback">
+                <?= (isset($error['name']))?$error['name']:'' ?>
+            </div>
+        </div>
+    </div>
+    
+    <div class="form-group mb-2 row">
+        <label class="sr-only col-sm-2 col-form-label" for="inlineFormInputGroup">Preis</label>
+        <div class="col-sm-5">
+            <div class="">
+                <input type="number" class="form-control <?= isset($error['price_per_unit'])?'is-invalid':'' ?>" id="price_per_unit" name="price_per_unit" placeholder="1" value="<?= isset($product['price_per_unit']) ? $product['price_per_unit'] : ''; ?>" required>
+                <div class="invalid-feedback"><?= (isset($error['price_per_unit']))?$error['price_per_unit']:'' ?></div>
+            </div>
+        </div>
+        <div class="col-sm-3">
+            <div class="input-group">
+                <select name="unit_symbol" id="unit_symbol" class="form-select" required>
+                    <? foreach($unit_symbols as $symbol): ?>
+                        <option <?= (isset($product['unit_symbol']) && $product['unit_symbol'] == $symbol) ? ' selected' : '' ?> value="<?=$symbol?>">
+                            <?= $symbol ?>
+                        </option>
+                    <?endforeach; ?>
+                </select>
+            </div>
+        </div>
+    </div>
 
-    <button type="submit">LOS</button>
+    <div class="form-group mb-2 row">
+        <label for="is_meal" class="col-sm-2 col-form-label">Typ</label>
+        <div class="col-sm-10">
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="is_meal" id="is_meal_0" value="0" <?= (!isset($product['is_meal'])) ? 'checked' :  (($product['is_meal'] == '0') ? 'checked' : '') ?>>
+                <label class="form-check-label" for="is_meal_0">Zutat</label>
+            </div>
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="is_meal" id="is_meal_1" value="1" <?= (isset($product['is_meal']) && $product['is_meal'] == '1') ? 'checked' : '' ?>>
+                <label class="form-check-label" for="is_meal_1">Gericht</label>
+            </div>
+            <div class="invalid-feedback">
+                <?= (isset($error['is_meal']))?$error['is_meal']:'' ?>
+            </div>
+        </div>
+    </div>
+
+    <div class="form-group mb-2 row">
+        <label for="image_path" class="col-sm-2 col-form-label">Bild</label>
+        <div class="col-sm-10">
+            <input type="text" class="form-control <?= isset($error['image_path'])?'is-invalid':'' ?>" id="image_path" name="image_path"
+                   placeholder="Pfad"
+                   value="<?= isset($product['image_path']) ? $product['image_path'] : ''; ?>" >
+            <div class="invalid-feedback">
+                <?= (isset($error['image_path']))?$error['image_path']:'' ?>
+            </div>
+        </div>
+    </div>
+
+    <div class="form-group mb-2 row">
+        <label for="enabled" class="col-sm-2 col-form-label">Typ</label>
+        <div class="col-sm-10">
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="enabled" id="enabled_0" value="0" <?= (isset($product['enabled']) && $product['enabled'] == '0') ? 'checked' : '' ?>>
+                <label class="form-check-label" for="enabled_0">Passiv</label>
+            </div>
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="enabled" id="enabled_1" value="1" <?= (!isset($product['enabled'])) ? 'checked' : (($product['enabled'] == '1') ? 'checked' : '') ?>>
+                <label class="form-check-label" for="enabled_1">Aktiv</label>
+            </div>
+            <div class="invalid-feedback">
+                <?= (isset($error['enabled']))?$error['enabled']:'' ?>
+            </div>
+        </div>
+    </div>
+
+    <button class="btn" type="submit" id="save_button">Speichern</button>
 
 </form>
 
 
 <?php if(isset($product)) : ?>
-<h3>Zutaten</h3>
+    
+<h3 style="margin-top:2em;">Zutaten</h3>
 <table class="table">
     <thead>
     <tr>
